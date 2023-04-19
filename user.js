772488347d410b04b029dff3843a83cf63ad04c5
cingfong <44254681+cingfong@users.noreply.github.com>
2023-04-19 17:31:46 +0800

    fix: 玩家倉庫-拖動效果

--- user.js
+++ user.js
@@ -1,11 +1,13 @@
-class Player {
-    constructor() {
+class User {
+    constructor(maxPieces, money, health) {
         this.maxPieces = maxPieces || 5; // 最大棋子數量
         this.money = money || 20; // 玩家擁有的金額
         this.health = health || 100; // 玩家的血量
-        this.pieces = new Array(9); // 玩家擁有的棋子
-        this.pieces.fill(null);
+        this.pieces = new Array(5); // 玩家擁有的棋子
+        this.pieces.fill(null)
+        this.pieces[0]='弓兵'
     }
+
 
     // 新增一個棋子
     addPiece(piece) {
@@ -25,39 +27,38 @@ class Player {
             console.log("You do not have this piece.");
             return;
         }
-
         this.pieces.splice(index, 1);
         console.log(`${piece.name} has been removed from your pieces.`);
     }
 
     // 顯示玩家擁有的棋子
     displayPieces() {
-        console.log("Current pieces:");
-        this.pieces.forEach((piece, index) => {
-            console.log(`[${index + 1}] ${piece.name} (Race: ${piece.race}, Attack: ${piece.attack}, Health: ${piece.health}, Effect: ${piece.effect})`);
-        });
+        return this.pieces
+        // this.pieces.forEach((piece, index) => {
+        //     console.log(`[${index + 1}] ${piece.name} (Race: ${piece.race}, Attack: ${piece.attack}, Health: ${piece.health}, Effect: ${piece.effect})`);
+        // });
     }
 }
-
+export default User
 // 創建一個玩家
-const player = new Player(8, 100, 1000);
+// const player = new Player(8, 100, 1000);
 
-// 新增一個棋子
-const piece1 = new ChessPiece("Piece 1", "Human", 30, 80, "Stun");
-player.addPiece(piece1);
+// // 新增一個棋子
+// const piece1 = new ChessPiece("Piece 1", "Human", 30, 80, "Stun");
+// player.addPiece(piece1);
 
-// 顯示玩家擁有的棋子
-player.displayPieces();
+// // 顯示玩家擁有的棋子
+// player.displayPieces();
 
-// 新增一個棋子
-const piece2 = new ChessPiece("Piece 2", "Elf", 35, 90, "Silence");
-player.addPiece(piece2);
+// // 新增一個棋子
+// const piece2 = new ChessPiece("Piece 2", "Elf", 35, 90, "Silence");
+// player.addPiece(piece2);
 
-// 顯示玩家擁有的棋子
-player.displayPieces();
+// // 顯示玩家擁有的棋子
+// player.displayPieces();
 
-// 移除一個棋子
-player.removePiece(piece1);
+// // 移除一個棋子
+// player.removePiece(piece1);
 
-// 顯示玩家擁有的棋子
-player.displayPieces();
+// // 顯示玩家擁有的棋子
+// player.displayPieces();
