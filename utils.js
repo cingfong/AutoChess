30d88ab49978f1247f259071e9112d3153c3582b
cingfong <44254681+cingfong@users.noreply.github.com>
2023-06-20 17:50:15 +0800

    fix: pop-ups ok

--- utils.js
+++ utils.js
@@ -1,5 +1,18 @@
 export default {
   popUps: ({ type, title, content }) => {
-    const Dom = document.querySelector("pop-up-card");
+    return new Promise((resolve, reject) => {
+      const wrap = document.querySelector(".pop-up-card");
+      const titleDom = wrap.querySelector(".title");
+      const contentDom = wrap.querySelector(".content");
+      titleDom.textContent = title;
+      contentDom.textContent = content;
+      const confirmBtn = wrap.querySelector(".button.confirm");
+      const clickMethod = () => {
+        wrap.classList.add("hidden");
+        confirmBtn.removeEventListener("click", clickMethod);
+        resolve();
+      };
+      confirmBtn.addEventListener("click", clickMethod);
+    });
   },
 };
